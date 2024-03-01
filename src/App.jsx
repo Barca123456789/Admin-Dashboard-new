8e81644e1b84f541a0c62b5d63da2c6ae6a88f84
Soukarya Raha <154533551+Barca123456789@users.noreply.github.com>
2024-03-01 20:40:16 +0530

    Files upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+import Sidebar from './assets/Sidebar'
+import Home from './assets/Home'
+import Header from './assets/Header'
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
+}
+
+export default App
